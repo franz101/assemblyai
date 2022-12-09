90f1a458ada1f1fe0691ae7f25d4196d640b93cf
franz.media <fkrekeler@gmail.com>
2022-12-09 16:03:40 -0500

    fix downloader

--- tube2blog/downloader.py
+++ tube2blog/downloader.py
@@ -3,7 +3,7 @@ from pytube import YouTube
 
 class Downloader:
     def __init__(self,url= "https://www.youtube.com/watch?v=NT2H9iyd-ms"):
-        youtube_video = YouTube(youtube_video_url)
+        youtube_video = YouTube(url)
         streams = youtube_video.streams.filter(only_audio=True)
         stream = streams.first()
         uid = uuid.uuid4()
