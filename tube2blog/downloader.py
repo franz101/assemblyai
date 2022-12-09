5747864defc6760ad64b18d6c0832c5ef9f1fd54
Franz Gusto <franz@rungalileo.io>
2022-12-09 18:46:41 -0500

    added demo data

--- tube2blog/downloader.py
+++ tube2blog/downloader.py
@@ -1,11 +1,19 @@
 import uuid
 from pytube import YouTube
+from moviepy.editor import *
+
 
 class Downloader:
-    def __call__(self,url= "https://www.youtube.com/watch?v=NT2H9iyd-ms") -> str:
+    def fetch_yt(self, url="https://www.youtube.com/watch?v=NT2H9iyd-ms") -> str:
         youtube_video = YouTube(url)
         streams = youtube_video.streams.filter(only_audio=True)
         stream = streams.first()
         uid = uuid.uuid4().hex
         stream.download(filename=uid)
         return uid
+
+    def convert_to_mp3(self, uid: str, dir: str = ".") -> str:
+        output_file = f"{dir}/{uid}.mp3"
+        audio = AudioFileClip(uid)
+        audio.write_audiofile(output_file)
+        return output_file
