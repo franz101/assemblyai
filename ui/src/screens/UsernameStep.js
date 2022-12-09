4c3d0dc4724a4e8c35f3c2e714471b997e6c686e
Part Time Larry <119373389+PartTimeAI@users.noreply.github.com>
2022-12-09 15:33:46 -0800

    add some error handling on handle submission

--- ui/src/screens/UsernameStep.js
+++ ui/src/screens/UsernameStep.js
@@ -1,21 +1,33 @@
+import { useState } from 'react';
 import { Container, Typography, TextField, Button } from '@mui/material';
 
 const UsernameStep = () => {
+    const [error, setError] = useState(false);
 
-    const submitUsername = () => {
-        const username = document.getElementById('username').value;
-        fetch(`http://127.0.0.1:5000/verify_handle/${username}`)
+    const verifyHandle = () => {
+        const handle = document.getElementById('handle').value;
+        if (handle[0] !== '@') {
+            // must use handle that starts with @
+            setError(true);
+            return;
+        }
+
+        fetch(`http://127.0.0.1:5000/verify_handle/${handle}`)
             .then((r) => r.json())
             .then((response) => {
                 console.log(response);
             })
+            .catch(function(error) {
+                console.log('Request failed', error);
+                setError(true);
+            });
     }
 
     return <Container>
         <Typography>https://youtube.com/</Typography>
-        <TextField id="username" label="@yourchannel" variant="outlined" />
+        <TextField id="handle" label="@yourchannel" variant="outlined" error={error} />
         <Container>
-            <Button onClick={submitUsername} variant="outlined">Verify</Button>
+            <Button onClick={verifyHandle} variant="outlined">Verify</Button>
         </Container>
     </Container>
 }
