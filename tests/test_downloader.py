b6ae9f9adc40c530371df6126ba0089e5f77f91e
franz.media <fkrekeler@gmail.com>
2022-12-09 16:15:21 -0500

    added downloader callable

--- tests/test_downloader.py
+++ tests/test_downloader.py
@@ -2,6 +2,7 @@ from tube2blog.downloader import Downloader
 import os
 
 def test_assembly_downloader():
-    url = Downloader()
+    d = Downloader()
+    url = d()
     assert url is not None
     
--- tube2blog/downloader.py
+++ tube2blog/downloader.py
@@ -2,7 +2,7 @@ import uuid
 from pytube import YouTube
 
 class Downloader:
-    def __init__(self,url= "https://www.youtube.com/watch?v=NT2H9iyd-ms") -> str:
+    def __call__(self,url= "https://www.youtube.com/watch?v=NT2H9iyd-ms") -> str:
         youtube_video = YouTube(url)
         streams = youtube_video.streams.filter(only_audio=True)
         stream = streams.first()
