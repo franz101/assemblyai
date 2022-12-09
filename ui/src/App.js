23f9d3fac55c79913d8b10ed1b134c5aaa2ba730
Part Time Larry <119373389+PartTimeAI@users.noreply.github.com>
2022-12-09 12:05:01 -0800

    add mui, input textfield for home page

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,23 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import { Container, Typography, TextField} from '@mui/material';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Typography variant='h1'>tube2blog</Typography>
+
+      <Container>
+        https://youtube.com/
+        <TextField id="outlined-basic" label="@yourchannel" variant="outlined" />
+      </Container>
     </div>
   );
 }
