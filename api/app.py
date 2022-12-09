7ea7b2fa96ba45bd7fa8fe250d4fc2b9df82d720
Part Time Larry <119373389+PartTimeAI@users.noreply.github.com>
2022-12-09 14:49:54 -0800

    flask endpoints verify_channel, fetch_channel_videos

--- api/app.py
+++ api/app.py
@@ -1,18 +1,25 @@
 import requests, config
-from flask import Flask
+from flask import Flask, abort, request
 from googleapiclient.discovery import build
 from utils.youtube import get_channel_id_from_handle
 
 app = Flask(__name__)
 
-@app.route("/")
-def fetch_videos(handle="@parttimelarry"):
+@app.route("/verify_channel/<handle>")
+def verify_channel(handle="@parttimelarry"):
     channel_id = get_channel_id_from_handle(handle)
 
     if channel_id is None:
-        return {
-            "code": "error"
-        }
+        abort(500)
+
+    return {
+        "code": "success",
+        "channel_id": channel_id
+    }
+
+@app.route("/fetch_channel_videos/<channel_id>")
+def fetch_channel_videos(channel_id):
+    page = request.args.get('page', 1)
 
     url = f"https://www.googleapis.com/youtube/v3/search?key={config.YOUTUBE_API_KEY}&channelId={channel_id}&part=snippet,id&order=date&maxResults=50"
     r = requests.get(url)
