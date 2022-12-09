7150163b2514e53d8b1c6b3d5f6ec96a37378a46
franz.media <fkrekeler@gmail.com>
2022-12-09 15:50:24 -0500

    added further api calls

--- tube2blog/api.py
+++ tube2blog/api.py
@@ -1,23 +1,23 @@
 import requests
-
+import time
 
 class AssemblyAiAPI:
-    endpoint = "https://api.assemblyai.com/v2/transcript"
+    api_url = "https://api.assemblyai.com/v2"
 
     def __init__(self, API_KEY: str):
         self.create_headers(API_KEY)
 
     def create_headers(API_KEY:str):
-        self.header = {
-            "Authorization": API_KEY,
-            "Content-Type": "application/json"
-            }
+        self.header = { "Authorization": API_KEY }
 
-    def transcript(payload):
-        id = requests.get(endpoint, headers=headers)
+    def transcript(id:str):
+        r =  requests.get(f"{api_url}/transcript",
+        headers=self.headers)
+        return r.json()
 
-    def get_analysis():
+    def get_analysis(audio_url: str):
         payload = {
+            "audio_url": audio_url,
             "sentiment_analysis": True,
             "entity_detection": True,
             "auto_chapters": True,
@@ -28,18 +28,25 @@ class AssemblyAiAPI:
 
     def get_summary():
         payload = {
+            "audio_url" : audio_url,
             "summarization": True,
             "summary_type": "bullets",
             "summary_model":"informative"
         }
+
+    def upload_file(file_path):
+        r = requests.post(f'{self.api_url}/upload',
+                        headers=self.headers,
+                        data=read_file(filename))
+        j = r.json()
+        return j["upload_url"]
+
     
     def wait_for_job(id):
+        d = self.transcript(id)
+        while d.get("status") not in ["error", "completed"]:
+            time.sleep(1)
+            d = self.transcript(id)
+        return d
+        
 
-
-
-    username
-    workers
-
-
-    fetch status of this username_job_id:
-    
