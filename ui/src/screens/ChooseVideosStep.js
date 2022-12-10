6062cbb8aa70f6b7172419ed25178f7fffcd2950
Part Time Larry <119373389+PartTimeAI@users.noreply.github.com>
2022-12-09 16:23:51 -0800

    connect youtube api, implement material video chooser

--- ui/src/screens/ChooseVideosStep.js
+++ ui/src/screens/ChooseVideosStep.js
@@ -1,15 +1,36 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Container, Typography, TextField, Button } from '@mui/material';
 import VideoChooser from '../components/VideoChooser';
 
 const ChooseVideosStep = (props) => {
     const [error, setError] = useState(false);
+    const [videos, setVideos] = useState([]);
 
-    console.log('props for choose videos')
-    console.log(props);
+      useEffect(() => {
+        fetch(`http://127.0.0.1:5000//fetch_channel_videos//${props.data.channel_id}`)
+            .then((r) => r.json())
+            .then((response) => {
+                const videoRows = response.items.filter((row) => row.id.kind == 'youtube#video');
+                const rows = videoRows.map((row) => {
+
+                    return {
+                        id: row.id.videoId,
+                        thumbnail: row.snippet.thumbnails.medium.url,
+                        title: row.snippet.title,
+                        date: row.snippet.publishTime
+                    }
+                })
+
+                setVideos(rows);
+            })
+            .catch(function(error) {
+                console.log('Request failed', error);
+                setError(true);
+            });
+      }, []);
 
     return <Container>
-        <VideoChooser />
+        <VideoChooser videos={videos} />
     </Container>
 }
 
