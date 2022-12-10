45aa38e1cf52d0c4fb78731abd808570212e7d6e
Part Time Larry <parttimeai@gmail.com>
2022-12-10 23:57:18 +0000

    systemd config, nginx config, add api prefix to endpoints

--- api/app.py
+++ api/app.py
@@ -6,7 +6,7 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
-@app.get("/verify_handle/<handle>")
+@app.get("/api/verify_handle/<handle>")
 def verify_channel(handle="@parttimelarry"):
     channel_id = get_channel_id_from_handle(handle)
 
@@ -18,7 +18,7 @@ def verify_channel(handle="@parttimelarry"):
         "channel_id": channel_id
     }
 
-@app.get("/fetch_channel_videos/<channel_id>")
+@app.get("/api/fetch_channel_videos/<channel_id>")
 def fetch_channel_videos(channel_id):
     page = request.args.get('page', 1)
 
@@ -27,7 +27,7 @@ def fetch_channel_videos(channel_id):
    
     return r.json()
 
-@app.post("/enqueue_videos")
+@app.post("/api/enqueue_videos")
 def enqueue_videos():
     # accepts json post with video_ids
     data = request.json
